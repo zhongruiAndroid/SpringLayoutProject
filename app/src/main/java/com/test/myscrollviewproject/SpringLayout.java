3508bc4267c73038cf4038f69dda884d502f018b
zhongrui <zhongrui@021.com>
2020-05-14 10:03:17 +0800

    临时提交

--- app/src/main/java/com/test/myscrollviewproject/SpringLayout.java
+++ app/src/main/java/com/test/myscrollviewproject/SpringLayout.java
@@ -1,39 +1,52 @@
 package com.test.myscrollviewproject;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.view.NestedScrollingChild2;
 import android.support.v4.view.NestedScrollingParent2;
+import android.support.v4.view.NestedScrollingParentHelper;
 import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
 public class SpringLayout extends ViewGroup implements NestedScrollingParent2 {
+
+    private NestedScrollingParentHelper nestedScrollingParentHelper;
+
     public SpringLayout(Context context) {
         super(context);
+        init();
     }
+
 
     public SpringLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public SpringLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public SpringLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init();
     }
 
+    private void init() {
+        nestedScrollingParentHelper=new NestedScrollingParentHelper(this);
+    }
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int maxWidth = 0;
         int maxHeight = 0;
         int childCount = getChildCount();
@@ -69,26 +82,80 @@ public class SpringLayout extends ViewGroup implements NestedScrollingParent2 {
 
     @Override
     public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, @ViewCompat.ScrollAxis int axes, @ViewCompat.NestedScrollType int type) {
-        return false;
+        if(ViewCompat.SCROLL_AXIS_VERTICAL!=axes){
+            return false;
+        }
+        return !ViewCompat.canScrollVertically(target, 1);
     }
 
     @Override
     public void onNestedScrollAccepted(@NonNull View child, @NonNull View target, @ViewCompat.ScrollAxis int axes, @ViewCompat.NestedScrollType int type) {
-
+        nestedScrollingParentHelper.onNestedScrollAccepted(child,target,axes,type);
     }
 
     @Override
     public void onStopNestedScroll(@NonNull View target, @ViewCompat.NestedScrollType int type) {
-
-    }
-
-    @Override
-    public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, @ViewCompat.NestedScrollType int type) {
-
+        nestedScrollingParentHelper.onStopNestedScroll(target,type);
     }
 
     @Override
     public void onNestedPreScroll(@NonNull View target, int dx, int dy, @NonNull int[] consumed, @ViewCompat.NestedScrollType int type) {
 
+    }
+    @Override
+    public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, @ViewCompat.NestedScrollType int type) {
+
+    }
+
+
+    public static class LayoutParams extends MarginLayoutParams {
+        public LayoutParams(@NonNull Context c, @Nullable AttributeSet attrs) {
+            super(c, attrs);
+        }
+
+        public LayoutParams(int width, int height) {
+            super(width, height);
+        }
+
+        public LayoutParams(int width, int height, int gravity) {
+            super(width, height);
+        }
+
+        public LayoutParams(@NonNull ViewGroup.LayoutParams source) {
+            super(source);
+        }
+
+        public LayoutParams(@NonNull ViewGroup.MarginLayoutParams source) {
+            super(source);
+        }
+
+        public LayoutParams(@NonNull LayoutParams source) {
+            super(source);
+        }
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new LayoutParams(getContext(), attrs);
+    }
+
+    @Override
+    protected boolean checkLayoutParams(ViewGroup.LayoutParams p) {
+        return p instanceof LayoutParams;
+    }
+
+    @Override
+    protected ViewGroup.LayoutParams generateLayoutParams(ViewGroup.LayoutParams lp) {
+        if (lp instanceof LayoutParams) {
+            return new LayoutParams((LayoutParams) lp);
+        } else if (lp instanceof MarginLayoutParams) {
+            return new LayoutParams((MarginLayoutParams) lp);
+        }
+        return new LayoutParams(lp);
+    }
+
+    @Override
+    protected LayoutParams generateDefaultLayoutParams() {
+        return new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
     }
 }
