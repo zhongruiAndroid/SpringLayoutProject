b15b64fe29a260fce8767e70591dc5423ea8ed44
zhongrui <zhongrui@021.com>
2020-05-14 10:03:58 +0800

    临时提交

--- app/src/main/java/com/test/myscrollviewproject/MainActivity.java
+++ app/src/main/java/com/test/myscrollviewproject/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Scroller;
 
 public class MainActivity extends AppCompatActivity {
-    private String b="aaabbbccc";
+    private String b="aaabbb";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
